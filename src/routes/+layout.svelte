86912d175c935b4b2a43efb8662b6de9085b58ef
jbcarreon123 <jbcarreon212@gmail.com>
2025-03-14 23:34:45 +0800

    Add index page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,44 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import { fade } from 'svelte/transition';
+    import { page } from '$app/state';
 </script>
 
-{@render children()}
+<style>
+	@media (width >= 48rem) {
+		#main-container {
+			grid-template-columns: 75% 25%;
+		}
+	}
+</style>
+
+<div id="main-container" class="grid gap-2">
+	<main class="border-2 p-3 order-2 md:order-1" in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>
+		{@render children()}
+	</main>
+	<aside class="border-2 p-3 order-1 md:order-2 flex justify-between md:justify-normal md:flex-col gap-2">
+		<div id="links" class="flex flex-col">
+			<a href="/">Home <i class="text-grv-subtext0">(you are here)</i></a>
+			<a href="/api">API Docs</a>
+		</div>
+		<div id="user" class="text-right md:pt-5 md:text-left">
+			<p>Oh hello there, JB!</p>
+			<a href="/dash">Dashboard</a>
+			<a href="/cfg">Settings</a>
+			<a href="/">Log out</a>
+		</div>
+		<div id="check-out" class="pt-5 hidden md:block">
+			<p>Check out these resources!</p>
+			<a href="https://loveberry.nekoweb.org" target="_blank">Loveberry</a>
+		</div>
+	</aside>
+</div>
+
+<footer class="pt-5 w-full text-center">
+	Made by 
+	<a href="https://jbcarreon123.nekoweb.org" target="_blank">jbcarreon123</a>
+	and 
+	<a href="https://maxpixels.moe" target="_blank">maxpixels</a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class=" max-w-[75%]">
+    <h1 class="text-7xl md:text-8xl font-semibold">API Tools for Indie Web Developers</h1>
+    <p class="pt-6 pl-1">
+        jAPI is a set of tools and endpoints that makes dynamic data possible on
+        statically hosted indie websites. It's absolutely free and runs on donations!
+    </p>
+</div>
+<div class="pt-7">
+    <h2 class="text-4xl md:text-5xl font-semibold">What do we offer?</h2>
+    <div class="gap-4 pt-3 md:flex">
+        <div class="max-w-xs p-2 border-2 border-grv-overlay0">
+            <h2 class="text-3xl md:text-4xl font-semibold">Comments</h2>
+            <p>A quick and easy way to implement comments for your site</p>
+        </div>
+        <div class="max-w-xs p-2 border-2 border-grv-overlay0">
+            <h2 class="text-3xl md:text-4xl font-semibold">Webring Redirect</h2>
+            <p><i class="text-grv-subtext0"> (soon!)</i> An alternative way to redirect people for your webring, without involving any client-side JavaScript</p>
+        </div>
+    </div>
+</div>
